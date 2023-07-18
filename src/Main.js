aa44fc6b694b1015814aafa1e796a8a8cee76b1e
ShirajShrestha <shresthashiraj333@gmail.com>
2023-07-18 21:57:07 +0545

    first commit

---
+++ src/Main.js
@@ -0,0 +1,36 @@
+import axios from 'axios'
+import { useEffect, useState } from "react"
+import './style.css'
+
+
+function Main() {
+  const [items, setItems] = useState([])
+  useEffect(()=>{
+    axios.get('https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood')
+    .then(res=>{
+      setItems(res.data.meals)
+    })
+    .catch((err)=>{
+      console.log(err) 
+    })
+  },[])
+
+
+  const itemsList = items.map(({strMeal, strMealThumb, idMeal})=>{
+    return (
+      <>
+          <section className="card">
+              <img src={strMealThumb} />
+              <section className="content">
+                <p>{strMeal} </p>
+                <p> Id: {idMeal}</p>
+              </section>
+          </section>
+      </>
+    )
+  })
+
+  return <div className="items-container">{itemsList}</div>
+}
+
+export default Main
